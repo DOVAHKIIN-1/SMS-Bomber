80bc929b53f95abfafe3e18c2f97c9ad8783328b
DOVAHKIIN <117450197+DOVAHKIIN-1@users.noreply.github.com>
2023-01-02 11:09:43 +0330

    Add files via upload

---
+++ install.py
@@ -0,0 +1,25 @@
+import os 
+#≈≈≈≈≈≈≈≈≈≈≈
+l='\033[0;37m'        
+g='\033[32;1m' 
+y='\033[1;33m'
+w='\033[1;37m'
+p='\033[1;35m'
+#=====================================
+os.system("clear")
+print(f"\n\t {g}installing... {w}\n")
+os.system("apt install python -y")
+os.system("apt install python2 -y")
+os.system("apt install python3 -y")
+os.system("python get-pip.py")
+os.system("pip install urllib")
+os.system("pip install urllib3")
+os.system("pip install termcolor")
+os.system("pip install pyfiglet")
+os.system("clear")
+#=====================================
+print(f'\n \t  {p}[+] {l}NOW TYPE {g}python Sms-Bomber.py{y} \n ')
+
+# GitHub : https://github.com/DOVAHKIIN-1
+#Telegram : https://t.me/darkness_hackerpy
+# Coder : DOVAHKIIN
